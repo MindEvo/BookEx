aa1ea4b52dce5a7440ddff20668d37f7286792c3
MindEvo <35351786+MindEvo@users.noreply.github.com>
2022-04-03 15:46:51 -0700

    Initial commit

---
+++ bookMng/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+# Create your views here.
+
+from django.http import HttpResponse
+from .models import MainMenu
+from .forms import BookForm
+from django.http import HttpResponseRedirect
+from .models import Book
+
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request,
+                  "bookMng/index.html",
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
+
+
+@login_required(login_url=reverse_lazy('login'))
+def postbook(request):
+    submitted = False
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save(commit=False)
+            try:
+                book.username = request.user
+            except Exception:
+                pass
+            book.save()
+            return HttpResponseRedirect('/postbook?submitted=True')
+    else:
+        form = BookForm()
+        if 'submitted' in request.GET:
+            submitted = True
+        return render(request,
+                  "bookMng/postbook.html",
+                  {
+                      'form': form,
+                      'item_list': MainMenu.objects.all(),
+                      'submitted': submitted,
+                  }
+                  )
+
+
+@login_required(login_url=reverse_lazy('login'))
+def displaybooks(request):
+    books = Book.objects.all()
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+
+    return render(request,
+                    "bookMng/displaybooks.html",
+                    {
+                        'item_list': MainMenu.objects.all(),
+                        'books': books
+                    }
+                    )
+
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
